745f0f6001bdff19d5967c0e3f9696d9ce100915
TheTrunk <tadeas.kmenta@zel.cash>
2024-09-20 11:33:34 +0700

    automate

---
+++ inactivate.js
@@ -0,0 +1,33 @@
+// Make accounts inactive if their access time is more than one week ago
+const nodecmd = require('node-cmd');
+const util = require('util');
+
+const cmdAsync = util.promisify(nodecmd.get);
+
+async function start() {
+  try {
+    // get list 
+    const currentTime = Math.round(new Date().getTime() / 1000);
+    const weekAgo = currentTime - 7 * 24 * 60 * 60;
+    const execA = '~/monero-lws-trunk/build/src/monero-lws-admin list_accounts';
+    const cmdresA = await cmdAsync(execA);
+    console.log(cmdresA);
+    const parsed = JSON.parse(cmdresA);
+    const active = parsed.active;
+    const activeOLD = active.filter((acc) => acc.access_time < weekAgo);
+    for (const acc of activeOLD)  {
+      // move it to inactive
+      const execB = `~/monero-lws-trunk/build/src/monero-lws-admin modify_account_status inactive ${acc.address}`;
+      const cmdresB = await cmdAsync(execB);
+      console.log(cmdresB);
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setTimeout(() => {
+      start();
+    }, 60 * 60 * 1000) // run every hour
+  }
+}
+
+start();
